a2f1ec0f96762e680c2bbf24f4c3228193436ce3
Eric Porter <160357055+EPORTER34@users.noreply.github.com>
2024-12-03 10:52:49 -0800

    Removing Unnecessary code from enemy class
    changed inheritance from hitBox to sprite since the velocity isn't needed, removed multiple move functions that were no longer needed.

--- PAC_Invaders/PAC_Invaders/Enemy.hpp
+++ PAC_Invaders/PAC_Invaders/Enemy.hpp
@@ -2,10 +2,9 @@
 #include "hitBox.hpp"
 #include "Projectile.hpp"
 
-class Enemy : public HitBox
+class Enemy : public Sprite
 {
 public:
-	Enemy(Vector2f& newVelocity, int newHealth);
 	Enemy();
 	int getHealth();
 	void setHealth(int newHealth);
